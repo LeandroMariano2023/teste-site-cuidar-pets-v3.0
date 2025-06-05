1b4aac95fae39612d402aa6af4bda735f24ab419
LeandroMariano2023 <153309185+LeandroMariano2023@users.noreply.github.com>
2025-06-05 13:39:43 -0300

    aualizacao de cuidador

---
+++ castro_cuidador/script.js
@@ -0,0 +1,30 @@
+function validarSenha() {
+      const senha = document.getElementById("senha").value;
+      const confirmar = document.getElementById("confirmar_senha").value;
+      if (senha !== confirmar) {
+        alert("As senhas não coincidem.");
+        return false;
+      }
+      return true;
+    }
+
+    function validarSenha() {
+      const senha = document.getElementById("senha").value;
+      const confirmar = document.getElementById("confirmar_senha").value;
+      if (senha !== confirmar) {
+        alert("As senhas não coincidem.");
+        return false;
+      }
+      return true;
+    }
+
+    function toggleSenha(id, elemento) {
+      const input = document.getElementById(id);
+      if (input.type === "password") {
+        input.type = "text";
+        elemento.textContent = "🙈"; // ícone alternativo
+      } else {
+        input.type = "password";
+        elemento.textContent = "👁️";
+      }
+    }
